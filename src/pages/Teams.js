21eac69b62f7ffa33dd6840bb24fe04e4f3c3500
averyB <averwhy@gmail.com>
2022-02-23 13:49:32 -0500

    start teams layout

--- src/pages/Teams.js
+++ src/pages/Teams.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {ListGroup, ListGroupItem, ListGroupItemHeading, Container, Badge, UncontrolledAlert, Popover, PopoverHeader, PopoverBody} from "reactstrap";
 
 const Teams = (props) => {
-return (
-    <div>
-      Teams
-    </div>
+  return (
+  <Container>
+  <UncontrolledAlert color="warning">Hey! This is just example data to test the components, not real team data.</UncontrolledAlert>
+  <br></br>
+  <h2>Team List</h2>
+  <ListGroup flush>
+    <ListGroupItem
+      action
+      tag="button">
+      <ListGroupItemHeading>Team 138</ListGroupItemHeading>
+      <Badge pill color="success">Hightest climber average</Badge>
+    </ListGroupItem>
+    <ListGroupItem
+      action
+      tag="button">
+      <ListGroupItemHeading>Team 256</ListGroupItemHeading>
+      <Badge pill color="success">Highest auto points</Badge>
+    </ListGroupItem>
+  </ListGroup>
+</Container>
   )
 };
 
