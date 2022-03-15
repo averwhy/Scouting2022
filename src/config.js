1f33d233cdd65e6b5dbbc5a5246017809df0468a
averyB <averwhy@gmail.com>
2022-03-14 22:09:04 -0400

    Create config.js template

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    firebase_key: "",
+    firebase_auth_domain: "",
+    firebase_project_id: "",
+    firebase_bucket: "",
+    firebase_messagingSenderId: "",
+    firebase_appId: "",
+    firebase_measurementId: "",
+}
+    
+export default config;
