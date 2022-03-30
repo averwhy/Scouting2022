50b935f9b9da7506ee56fe22f4ec052dea3d58e9
averyB <averwhy@gmail.com>
2022-03-30 19:43:01 -0400

    Remove tokens
    lol. screw you github desktop

--- src/config.js
+++ src/config.js
@@ -1,13 +1,14 @@
 const config = {
-    firebase_key: "AIzaSyD5X24NBJeo70gDK9UD3eGnLbxnqzmcGgk",
-    firebase_auth_domain: "scouting2022-a977d.firebaseapp.com",
-    firebase_project_id: "scouting2022-a977d",
-    firebase_bucket: "scouting2022-a977d.appspot.com",
-    firebase_messagingSenderId: "336313340843",
-    firebase_appId: "1:336313340843:web:c14d64c619b9c949c6b8ee",
-    firebase_measurementId: "G-D4NBV84TMR",
+    firebase_key: "",
+    firebase_auth_domain: "",
+    firebase_project_id: "",
+    firebase_bucket: "",
+    firebase_messagingSenderId: "",
+    firebase_appId: "",
+    firebase_measurementId: "",
 
-    database_collection: "shrewsbury-real"
+    database_collection: "example-database",
+    database_pit_suffix: "-pit"
 }
     
 export default config;
